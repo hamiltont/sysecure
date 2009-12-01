3e4c7d14d18d98295270a1475e288512a125f980
jason.r.cody <jason.r.cody@82127ed8-cfcd-11de-97bf-6350044336de>
2009-12-01 18:46:03 +0000

    Fixed bug in pub_key.c.  pub_key:init_pub_key now generates a public key pair and adds it to the key ring.  TODO: add a get_pub_key function that takes in an id and returns a key.

--- pub_key.c
+++ pub_key.c
@@ -13,38 +13,44 @@
 
 GList* SYS_key_ring = NULL;
 
-void generate_RSA_Key_Pair (RSA_Key_Pair* temp_key)
+void generate_RSA_Key_Pair (RSA_Key_Pair** temp_key)
 {
   PK11SlotInfo *slot = 0;
   PK11RSAGenParams rsaParams;
   rsaParams.keySizeInBits = 1024;
   rsaParams.pe = 65537;
-  temp_key = malloc (sizeof(RSA_Key_Pair));
-  temp_key->priv = 0;
-  temp_key->pub = 0;
+  *temp_key = malloc (sizeof(RSA_Key_Pair));
   slot = PK11_GetInternalKeySlot();
-  temp_key->priv = PK11_GenerateKeyPair(slot, CKM_RSA_PKCS_KEY_PAIR_GEN, &rsaParams,
-               &(temp_key->pub), PR_TRUE, PR_TRUE, 0);
+
+  (*temp_key)->priv = PK11_GenerateKeyPair(slot, CKM_RSA_PKCS_KEY_PAIR_GEN, &rsaParams,
+               &((*temp_key)->pub), PR_FALSE, PR_FALSE, 0);
+
+  if ((*temp_key)->priv != NULL)
+    purple_debug(PURPLE_DEBUG_INFO, "SySecure", "generate_RSA_Key_Pair: priv Key Exists!");
+  else
+    purple_debug(PURPLE_DEBUG_INFO, "SySecure", "generate_RSA_Key_Pair: priv Key DOES NOT Exist!");
   return;
 }
 
 void init_pub_key (char* name)
 {
-  RSA_Key_Pair temp_key;
+  RSA_Key_Pair *temp_key;
   if (!SYS_key_ring)
   {
     generate_RSA_Key_Pair(&temp_key);
-    temp_key.id_name = malloc(strlen(name)*sizeof(char));
-    memset(temp_key.id_name, 0, strlen(name));
-    memcpy(temp_key.id_name, name, strlen(name) + 1);
-    SYS_key_ring = g_list_append(SYS_key_ring, &temp_key);
+    temp_key->id_name = malloc(strlen(name)*sizeof(char));
+    memset(temp_key->id_name, 0, strlen(name));
+    memcpy(temp_key->id_name, name, strlen(name) + 1);
+    SYS_key_ring = g_list_append(SYS_key_ring, temp_key);
     purple_debug(PURPLE_DEBUG_INFO, "SySecure", "New key created for: %s.\n",
-             temp_key.id_name);
+             temp_key->id_name);
     return;
   }
-  temp_key = *((RSA_Key_Pair*) SYS_key_ring->data);
-  purple_debug(PURPLE_DEBUG_INFO, "SySecure", "Key exists for: %s and it is: %s\n",
-             temp_key.id_name, temp_key.pub->u.rsa.modulus.data);
+  temp_key = (RSA_Key_Pair*)SYS_key_ring->data;
+  if (temp_key->pub != NULL)
+    purple_debug(PURPLE_DEBUG_INFO, "SySecure", "Key exists for : %s.\n", temp_key->id_name);
+  else
+    purple_debug(PURPLE_DEBUG_INFO, "SySecure", "Key does not exist.");
 }
 
 //Reference 1: Used REF 1 as a basis for nss_init()
