0a58887c063ec8bb0b97812c59d8a418e6560e52
jason.r.cody <jason.r.cody@82127ed8-cfcd-11de-97bf-6350044336de>
2009-12-01 16:59:08 +0000

    Added pub_key.c and pub_key.h to the server.

---
+++ pub_key.c
@@ -0,0 +1,68 @@
+#ifndef PUB_KEY_C
+#define PUB_KEY_C
+
+/*
+*References:
+*  1) William Tompkins, "Pidgin-Encryption: rsa_nss.c," 
+*     (http://pidgin-encrypt.sourceforge.net/install.php)
+*
+*/
+
+//Internal Includes
+#include "pub_key.h"
+
+GList* SYS_key_ring = NULL;
+
+void generate_RSA_Key_Pair (RSA_Key_Pair* temp_key)
+{
+  PK11SlotInfo *slot = 0;
+  PK11RSAGenParams rsaParams;
+  rsaParams.keySizeInBits = 1024;
+  rsaParams.pe = 65537;
+  temp_key = malloc (sizeof(RSA_Key_Pair));
+  temp_key->priv = 0;
+  temp_key->pub = 0;
+  slot = PK11_GetInternalKeySlot();
+  temp_key->priv = PK11_GenerateKeyPair(slot, CKM_RSA_PKCS_KEY_PAIR_GEN, &rsaParams,
+               &(temp_key->pub), PR_TRUE, PR_TRUE, 0);
+  return;
+}
+
+void init_pub_key (char* name)
+{
+  RSA_Key_Pair temp_key;
+  if (!SYS_key_ring)
+  {
+    generate_RSA_Key_Pair(&temp_key);
+    temp_key.id_name = malloc(strlen(name)*sizeof(char));
+    memset(temp_key.id_name, 0, strlen(name));
+    memcpy(temp_key.id_name, name, strlen(name) + 1);
+    SYS_key_ring = g_list_append(SYS_key_ring, &temp_key);
+    purple_debug(PURPLE_DEBUG_INFO, "SySecure", "New key created for: %s.\n",
+             temp_key.id_name);
+    return;
+  }
+  temp_key = *((RSA_Key_Pair*) SYS_key_ring->data);
+  purple_debug(PURPLE_DEBUG_INFO, "SySecure", "Key exists for: %s and it is: %s\n",
+             temp_key.id_name, temp_key.pub->u.rsa.modulus.data);
+}
+
+//Reference 1: Used REF 1 as a basis for nss_init()
+
+gboolean nss_init (void) 
+{
+  //gboolean nss_loaded = FALSE;
+  PurplePlugin *plugin = purple_plugins_find_with_name("NSS");
+  if (plugin)
+  {
+    purple_debug(PURPLE_DEBUG_INFO, "SySecure", "NSS is initialized.\n");
+    return TRUE;
+  }
+  else
+  {
+    purple_debug(PURPLE_DEBUG_ERROR, "SySecure", "NSS is not intitialized.\n");
+    return FALSE;
+  }
+}
+
+#endif //PUB_KEY_C
---
+++ pub_key.h
@@ -0,0 +1,33 @@
+#ifndef PUB_KEY_H
+#define PUB_KEY_H
+
+//LIBPURPLE Includes
+#include "plugin.h"
+#include "debug.h"
+
+//GLIB Includes
+#include "glib.h"
+
+//NSS includes:
+#include "nss.h"
+#include "nspr.h"
+#include <ssl.h>
+#include <secmod.h>
+#include <pk11func.h>
+#include <pk11pub.h>
+#include <keyhi.h>
+#include <nssb64.h>
+
+typedef struct {
+  char* id_name;
+  SECKEYPrivateKey* priv;
+  SECKEYPublicKey* pub;
+} RSA_Key_Pair;
+
+void generate_RSA_Key_Pair (RSA_Key_Pair* temp_key);
+
+void init_pub_key (char* name);
+
+gboolean nss_init (void);
+
+#endif //PUB_KEY_H
