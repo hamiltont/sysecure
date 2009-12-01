26e8de081a68d6e7067921f7023400071f4e7172
hamiltont <hamiltont@82127ed8-cfcd-11de-97bf-6350044336de>
2009-12-01 04:19:07 +0000

    Now map conversation names to EncryptionInfo, rather than the raw conversation pointer. This allows persistence across multiple conversations

--- conv_encrypt_map.c
+++ conv_encrypt_map.c
@@ -20,13 +20,26 @@
  * object will be created and returned. When the uninit method of this class 
  * is called, it cleans up the memory used, and has a change to store some 
  * information permanently (although right now it does not). 
- * TODO - add a getEncryptionInfo message so they can get the EncryptionInfo
- *        from the receiver name, as well as conversation. Because we are not 
- *        securing chats, this should always work 
+ * 
+ * Note that this class uses the conversation name as a key, and returns the 
+ * EncryptionInfo as a value. In other parts of the code base, it is assumed 
+ * that the conversation name == the receiver's SN, which seems to be true for
+ * IM's (not chats) as of this writing (pidgin 2.6.3)
+ *
+ * Also note that this class would need to be totally re-worked if this plugin
+ * were trying to support chats, and not just IM's!
+ *
+ * TODO - right now, this class uses a char * as the key. It would be a lot 
+ *        better to use a const char *!
+ *
+ * 
  */
 
 // Needed for a lot
 #include <glib.h>
+
+// string functions!
+#include <string.h>
 
 // libpurple debugging
 #include <debug.h>
@@ -46,31 +59,42 @@
 static GHashTable *conv_EI = NULL;
 
 /**
- * Retrieves the EncryptionInfo for a specific conversation. If the passed
- * conversation has no EncryptionInfo, a default EncryptionInfo is created and
+ * Retrieves the EncryptionInfo for a specific person. If the
+ * person has no EncryptionInfo, a default EncryptionInfo is created and
  * returned
+ *
+ * @param conversation_name The IM name of the person receiving the message
+ *
+ * TODO - this is not safe if someone has two buddies, on two accounts, that 
+ *        have the same IM screen name. Should fix this at some point 
  */
 EncryptionInfo *
-get_encryption_info(PurpleConversation *conv)
+get_encryption_info_from_name(const char * conversation_name)
 {
   EncryptionInfo *e_info;
   
   // Check to see if the EI exists yet
   e_info = (EncryptionInfo *)g_hash_table_lookup(conv_EI,
-                                                 conv);
+                                                 conversation_name);
   
   // Create it if it does not
   if (e_info == NULL)
     {
       purple_debug_info(PLUGIN_ID,
-                        "Conversation '%p' with title '%s' has no EncryptionInfo\n",
-                        conv,
-                        purple_conversation_get_name(conv));
+                        "Conversation with title '%s' has no EncryptionInfo\n",
+                        conversation_name);
+      
+      // Copy the conversation name, so we are guaranteed we don't loose our key
+      char *copy = malloc(strlen(conversation_name) * sizeof(char));
+      memset(copy,0,strlen(conversation_name));
+      strncpy(copy,conversation_name, strlen(conversation_name) + 1);
+      //purple_debug_info(PLUGIN_ID, "Turned %s into %s", conversation_name, copy);
+      
       
       // Create and insert into hash table
       e_info = init_encryption_info();
       g_hash_table_insert(conv_EI,
-                          conv,
+                          copy,
                           e_info);
                           
       purple_debug_info(PLUGIN_ID,
@@ -78,7 +102,21 @@ get_encryption_info(PurpleConversation *conv)
                         e_info);
     }
 
-  return e_info;  
+  return e_info;
+}
+
+/**
+ * Retrieves the EncryptionInfo for a specific conversation. If the passed
+ * conversation has no EncryptionInfo, a default EncryptionInfo is created and
+ * returned
+ *
+ * @param conv A PurpleConversation that you would like to retrieve the 
+ *             EncryptionInfo for. 
+ */
+EncryptionInfo *
+get_encryption_info(PurpleConversation *conv)
+{
+  return get_encryption_info_from_name(purple_conversation_get_name(conv));
 }
 
 /**
@@ -92,8 +130,7 @@ enable_encryption(PurpleConversation *conv)
   e_info->is_encrypted = TRUE;
   
   purple_debug_info(PLUGIN_ID, 
-		                "Enabled encryption on conversation '%p' with name '%s'\n",
-		                 conv,
+		                "Enabled encryption on conversation with name '%s'\n",
 		                 purple_conversation_get_name(conv));
 }
 
@@ -108,8 +145,7 @@ disable_encryption(PurpleConversation *conv)
   e_info->is_encrypted = FALSE;
   
   purple_debug_info(PLUGIN_ID, 
-		                "Disabled encryption on conversation '%p' with name '%s'\n",
-		                 conv,
+		                "Disabled encryption on conversation with name '%s'\n",
 		                 purple_conversation_get_name(conv));
 }
 
@@ -118,8 +154,8 @@ disable_encryption(PurpleConversation *conv)
  * Helper function that prints out information about every value stored in the 
  * hashtable currently.
  *
- * @param key The GHashTable key, currently an EncryptionInfo struct
- * @param value The GHashTable value, currently a PurpleConversation
+ * @param key The GHashTable key, currently a char *
+ * @param value The GHashTable value, currently an EncryptionInfo
  * @param user_data Any random user data that the calling function passes. 
  *        Currently this field is ignored. 
  */
@@ -127,18 +163,16 @@ static void
 debug_helper_foreach_cb(gpointer key, gpointer value, gpointer user_data) 
 {
   EncryptionInfo *enc = (EncryptionInfo*)value;
-  PurpleConversation *conv = (PurpleConversation*)key;
+  char *conv = (char *)key;
   
   if (enc->is_encrypted)
     purple_debug_info(PLUGIN_ID,
-			                "Conversation '%p' with name '%s' is encrypted\n",
-			                conv,
-			                purple_conversation_get_name(conv));
+			                "Conversation with name '%s' is encrypted\n",
+			                conv);
   else
     purple_debug_info(PLUGIN_ID,
-			                "Conversation '%p' with name '%s' is not encrypted\n",
-			                conv,
-			                purple_conversation_get_name(conv));
+			                "Conversation with name '%s' is not encrypted\n",
+			                conv);
 			                
 }
 
@@ -161,21 +195,23 @@ debug_conv_encrypt_map()
  */
 void 
 init_conv_encryption_map() {
-  conv_EI = g_hash_table_new(NULL, NULL);
+  conv_EI = g_hash_table_new(g_str_hash, g_str_equal);
 }
 
 static void
 memory_free_foreach_cb(gpointer key, gpointer value, gpointer user_data)
 {
   EncryptionInfo *enc = value;
-  PurpleConversation *conv = key;
+  char *receiver_name = key;
   
   uninit_encryption_info(enc);
   purple_debug_info(PLUGIN_ID,
-                    "Freed EncryptionInfo '%p' from conversation '%p' with name '%s'\n",
+                    "Freed EncryptionInfo '%p' from conversation with name '%s'\n",
                     enc,
-                    conv,
-                    purple_conversation_get_name(conv));
+                    receiver_name);
+                    
+  // clean up the memory we allocated to copy the conversation name
+  free(key);
 }
 
 /**
